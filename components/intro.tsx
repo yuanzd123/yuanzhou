6bcfabbb4828d5d7bc4f00aeb8290cc82fba1122
yuanzd123 <yuanzhou93@gmail.com>
2024-06-22 15:17:25 -0700

    change resume pdf link

--- components/intro.tsx
+++ components/intro.tsx
@@ -90,7 +90,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full mr-6 outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/Yuan Zhou's resume.pdf"
+          href="/Yuan-Zhou-resume.pdf"
           download
         >
           Download Resume{" "}
